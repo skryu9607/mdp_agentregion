9dc8d1db0ff9a89965fae1bda3396570030dca3d
HUGO RYU <78149686+skryu9607@users.noreply.github.com>
2022-08-23 20:24:08 +0900

    Another commmit

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
+import matplotlib.pyplot as plt
 import numpy as np
-print(np.random.random((6,2))*10)
-print(7/2)
-print(7%2)
+from matplotlib.animation import FuncAnimation
+import random
+class RTplot():
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.c = []
+        self.upCount = 0
+        self.fig = plt.figure(figsize=(12,6))
+        self.ax = plt.subplot()
+        plt.autoscale(enable = True)
+
+    def aniFunc(self, i):
+        self.addPoint([random.randrange(1,100),random.randrange(1,100)])
+        self.ax.cla()
+        self.ax.scatter(self.x,self.y,c = self.c)
+    
+    def addPoint(self,list):
+        if self.upcou
